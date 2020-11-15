cb3b2dd8f768be845212b2682ddb29095febdc16
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2020-11-15 10:50:22 -0800

    Add files via upload

---
+++ JavaLecture3.java
@@ -0,0 +1,33 @@
+package com.rit;
+import static java.lang.System.out;
+// import static java.lang.System.in;
+// import static java.lang.System.err;
+// import static java.lang.System.*;
+
+public class JavaLecture3 {
+
+    public static void main(String[] args) {
+
+        // java.lang.System.out.println("Hi Java");
+        // System.out.println("Hi Java");
+        out.println("Hi Java");
+
+        // java.lang   ---  Package
+        // System      ---  Class
+        // out         ---  Field
+        // println()   ---  Method
+
+        // com.rit     ---  Package
+        // Main        ---  Class
+        // main()      ---  Method
+
+        // Method / Function -> A block of code that performs a task
+        // Class   -> A container for related functions / methods
+        // Package -> A Group of related classes
+
+        // Naming Convention
+        // Classes  -> PascalNamingConvention
+        // Methods  -> camelNamingConvention
+
+    }
+}
