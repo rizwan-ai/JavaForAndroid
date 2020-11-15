336b34b408a0577029d659f62d14d1e9002cc098
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2020-11-15 09:19:33 -0800

    Add files via upload

---
+++ JavaLecture2.java
@@ -0,0 +1,18 @@
+// package com.rit;
+
+public class JavaLecture2 {
+
+    public static void main(String[] args) {
+
+        java.lang.System.out.println("Hi Java");
+        // jdk-15      ---  JDK
+        // java.base   ---  Defines the foundational APIs of the Java SE Platform
+        // java        ---  Package
+        // lang        ---  Sub Package 
+        // System      ---  Class
+        // out         ---  Field
+        // println()   ---  Method
+        // "Hi Java"   ---  String Literal
+
+    }
+}
