664547575218fe42b29acc6f3e06c671f8b1520a
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2020-11-15 08:49:32 -0800

    Update JavaLecture1.java

--- JavaLecture1.java
+++ JavaLecture1.java
@@ -1,6 +1,6 @@
-package com.rit;
+// package com.rit;
 
-public class Main {
+public class JavaLecture1 {
 
     public static void main(String[] args) {
         java.lang.System.out.println("Welcome to Java Programming :)");
