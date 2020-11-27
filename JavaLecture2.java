71909c0d73ee591fc3a56ed1fd644813dbe60bd4
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2020-11-27 08:02:19 -0800

    Update JavaLecture2.java

--- JavaLecture2.java
+++ JavaLecture2.java
@@ -6,9 +6,10 @@ public class JavaLecture2 {
 
         java.lang.System.out.println("Hi Java");
         // jdk-15      ---  JDK (Java Development Kit)
-        // java.base   ---  Defines the foundational APIs of the Java SE Platform
+        // java.base   ---  Module (Defines the foundational APIs of the Java SE Platform) 
         // java        ---  Package
         // lang        ---  Sub Package 
+        // java.lang   ---  Pacakge (Provides classes that are fundamental to the design of the Java Programming Language.)
         // System      ---  Class
         // out         ---  Field
         // println()   ---  Method
